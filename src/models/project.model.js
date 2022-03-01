9e3c46b2f9c98d76ba28937607c9917a5d33cb66
arnWolff <arnwolff@gmail.com>
2022-03-01 20:16:20 +0100

    main #1

---
+++ src/models/project.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+
+const projectSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      private: true, // used by the toJSON plugin
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+projectSchema.plugin(toJSON);
+projectSchema.plugin(paginate);
+
+/**
+ * Check if name is taken
+ * @param {string} name - The project's name
+ * @param {ObjectId} [excludeProjectId] - The id of the project to be excluded
+ * @returns {Promise<boolean>}
+ */
+projectSchema.statics.isNameTaken = async function (name, excludeProjectId) {
+  const project = await this.findOne({ name, _id: { $ne: excludeProjectId } });
+  return !!project;
+};
+
+/**
+ * @typedef Project
+ */
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
